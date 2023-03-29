3a25fcd02d212aed1c142a131b5923157f533818
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-03-29 13:15:34 +0300

    update routes update seeds add a cors gem

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Formula1Rally
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post,:patch, :delete, :options]
+      end
+    end
+    end
 end
